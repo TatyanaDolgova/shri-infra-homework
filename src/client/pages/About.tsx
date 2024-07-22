9299bc5da6f605361d606ba382501d508830f34e
nchertolin <nchertolin@gmail.com>
2024-07-22 14:54:28 +0500

    feat: add inspector to about page

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,15 +5,16 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 
 export const About: React.FC = () => {
-  return (
-    <div className={bem()}>
-      <Helmet title="About" />
-      <div className="row">
-        <div className="col">
-          <h1>About</h1>
-          <p>Author: Tatiana Dolgova</p>
+    return (
+        <div className={bem()}>
+            <Helmet title="About" />
+            <div className="row">
+                <div className="col">
+                    <h1>About</h1>
+                    <p>Author: Tatiana Dolgova</p>
+                    <p>Inspector: Nikita Chertolin</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
